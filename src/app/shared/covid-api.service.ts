e98612668ce59c9d8a65fd59fc3b0070762aeb63
vishal011117 <vishal.narewade@quetzal.in>
2020-05-05 15:20:50 +0530

    fix: remove unwanted code

--- src/app/shared/covid-api.service.ts
+++ src/app/shared/covid-api.service.ts
@@ -6,17 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CovidApiService {
-  link = 'https://api.covid19india.org/data.json';
-  confirmLink = `${this.link}/status/confirmed`;
-  startDate = '2020-05-04T00:00:00Z';
-  currentTime = new Date(new Date().setHours(0, 0, 0, 0)).toISOString();
-  apiLink = '';
-
   constructor(  
     private http: HttpClient
-  ) {
-    this.apiLink = `${this.link}?from=${this.startDate}&to=${this.currentTime}`;
-  }
+  ) {}
 
   public getList(): Observable<any> {
     return this.http.get(`https://api.covid19india.org/data.json`);
