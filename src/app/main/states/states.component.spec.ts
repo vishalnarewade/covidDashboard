bd7115a6922b5eda2fea8199a96e7cb7988d489e
vishalnarewade <vishal.narewade@gmail.com>
2022-05-10 23:16:51 +0530

    fix update angular 10 to 11

--- src/app/main/states/states.component.spec.ts
+++ src/app/main/states/states.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StatesComponent } from './states.component';
 
@@ -6,7 +6,7 @@ describe('StatesComponent', () => {
   let component: StatesComponent;
   let fixture: ComponentFixture<StatesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StatesComponent ]
     })
